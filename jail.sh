9c44c1903e4c76790bdcbd23109edc3d749a3f63
Noaman <noamanahmd@gmail.com>
2022-04-15 01:29:41 +0500

    Fixed Jailed User bugs

--- jail.sh
+++ jail.sh
@@ -11,16 +11,21 @@ chroot_path=/var/chroot
 if [ -d "$chroot_path" ]
 then
     echo "Chroot exists as $chroot_path. First delete it!"
+    exit
 fi
 
 chown root:root /
 chown root:root /home
 
 mkdir -p $chroot_path
-mknod -m 666 $chroot_path/null c 1 3
-mknod -m 666 $chroot_path/tty c 5 0
-mknod -m 666 $chroot_path/zero c 1 5
-mknod -m 666 $chroot_path/random c 1 8
+
+chroot_dev_path="$chroot_path/dev"
+mkdir -p $chroot_dev_path
+
+mknod -m 666 $chroot_dev_path/null c 1 3
+mknod -m 666 $chroot_dev_path/tty c 5 0
+mknod -m 666 $chroot_dev_path/zero c 1 5
+mknod -m 666 $chroot_dev_path/random c 1 8
 
 chown root:root $chroot_path
 chmod 755 /var/chroot
@@ -28,18 +33,18 @@ chmod 755 /var/chroot
 chroot_bin_path="$chroot_path/bin"
 mkdir -p $chroot_bin_path
 
-cp /bin/bash $chroot_bin_path/
-cp -v /bin/ls $chroot_bin_path/bin/
-cp -v /bin/date $chroot_bin_path/bin/
-cp -v /bin/mkdir $chroot_bin_path/bin/
+cp -v /bin/bash $chroot_bin_path
+cp -v /bin/ls $chroot_bin_path
+cp -v /bin/date $chroot_bin_path
+cp -v /bin/mkdir $chroot_bin_path
 
 #Symbolic Links
-ln -s /usr/bin/php $chroot_bin_path/bin/php
-ln -s /usr/bin/git $chroot_bin_path/bin/git
-ln -s /usr/bin/wget $chroot_bin_path/bin/wget
-ln -s /usr/bin/composer $chroot_bin_path/bin/composer
-ln -s /usr/bin/composer1 $chroot_bin_path/bin/composer1
-ln -s /usr/bin/composer2 $chroot_bin_path/bin/composer2
+ln -s /usr/bin/php $chroot_bin_path/php
+ln -s /usr/bin/git $chroot_bin_path/git
+ln -s /usr/bin/wget $chroot_bin_path/wget
+ln -s /usr/bin/composer $chroot_bin_path/composer
+ln -s /usr/bin/composer1 $chroot_bin_path/composer1
+ln -s /usr/bin/composer2 $chroot_bin_path/composer2
 
 
 mkdir -p "$chroot_path/lib/x86_64-linux-gnu" "$chroot_path/lib64"
