ec53e543abcac097ef2b9f799144488a1bf365b7
Noaman <noamanahmd@gmail.com>
2022-04-15 00:05:27 +0500

    Added bashrc and profile

--- scripts.sh
+++ scripts.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+DIR=$(dirname "${BASH_SOURCE[0]}") 
+DIR=$(realpath "${DIR}") 
+
+template_path="$(cd $DIR && pwd)/templates"
+
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
 fi
 
-
-
+cp -rf template_path/.profile $HOME/.profile
+cp -rf template_path/.bashrc $HOME/.bashrc
+source $HOME/.profile
+source $HOME/.bashrc
