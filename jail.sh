9f8ad9bd7389e7c19780f1d50dc63404dce39008
Noaman <noamanahmd@gmail.com>
2022-04-18 02:45:52 +0500

    removed php from jail.sh

--- jail.sh
+++ jail.sh
@@ -57,7 +57,7 @@ mkdir -p "$chroot_path/lib/x86_64-linux-gnu" "$chroot_path/lib64"
 # cp /lib64/ld-linux-x86-64.so.2 "$chroot_path/lib64"
 # cp /lib/x86_64-linux-gnu/{libselinux.so.1,libcap.so.2,libacl.so.1,libc.so.6,libpcre2-8.so.0,libdl.so.2,ld-linux-x86-64.so.2,libattr.so.1,libpthread.so.0} "$chroot_path/lib/x86_64-linux-gnu"
 
-binaries_array=("xterm" "ls" "date" "rm" "rmdir" "mysql" "php" "php73" "php74" "php80" "php81" "git" "wget" "curl" "nano" "stty" "grep" "find" "clear" "du" "cp" "mv" "touch" "cat" "whoami" "tee" "free" "gdb" "mkdir" "git-shell" "git-receive-pack" "git-upload-archive" "git-upload-pack" "ping" "node")
+binaries_array=("xterm" "ls" "date" "rm" "rmdir" "mysql" "php73" "php74" "php80" "php81" "git" "wget" "curl" "nano" "stty" "grep" "find" "clear" "du" "cp" "mv" "touch" "cat" "whoami" "tee" "free" "gdb" "mkdir" "git-shell" "git-receive-pack" "git-upload-archive" "git-upload-pack" "ping" "node")
 
 for binary in ${binaries_array[@]}; do
     cp "$(which $binary)" $chroot_bin_path
