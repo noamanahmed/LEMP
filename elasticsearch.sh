7db69a76fe2c511bff60c9ba5476857c524589fe
Noaman <noamanahmd@gmail.com>
2022-04-18 17:41:07 +0500

    Fixed elastic search duplication sources.list issue

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -16,6 +16,7 @@ fi
 
 
 curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+rm -rf /etc/apt/sources.list.d/elastic-7.x.list
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt update
 apt install elasticsearch -y
