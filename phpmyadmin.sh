321ff26171824e2d77a48abd0a9b2b7dec6698e4
Noaman <noamanahmd@gmail.com>
2022-04-20 22:17:13 +0500

    Fixed templates path

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -3,7 +3,7 @@
 DIR=$(dirname "${BASH_SOURCE[0]}") 
 DIR=$(realpath "${DIR}") 
 
-template_path="$(cd $DIR/../ && pwd)/templates"
+template_path="$(cd $DIR && pwd)/templates"
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -25,6 +25,6 @@ fi
 
 
 wget https://files.phpmyadmin.net/phpMyAdmin/5.1.1/phpMyAdmin-5.1.1-all-languages.zip -O /tmp/phpmyadmin.zip
-unzip /tmp/phpmyadmin.zip -d /tmp/
+unzip -f /tmp/phpmyadmin.zip -d /tmp/
 mv /tmp/phpMyAdmin-5.1.1-all-languages /home/$username/www/phpmyadmin
 cp -rf $template_path/phpmyadmin/* /home/$username/www/phpmyadmin/
