3410a036f628f6f7f4e2910ac242a25d35abef05
Noaman <noamanahmd@gmail.com>
2022-04-20 22:46:12 +0500

    Fixed rabbitmq

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+DIR=$(dirname "${BASH_SOURCE[0]}") 
+DIR=$(realpath "${DIR}") 
+
+template_path="$(cd $DIR && pwd)/templates"
+
+
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
