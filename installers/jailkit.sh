5123dc1914c44fd6169a4cbaf65dc586e9ab975e
Noaman <noamanahmd@gmail.com>
2022-04-25 21:01:17 +0500

    Quick Push f59558f5e3cd5f4508368d9b

--- installers/jailkit.sh
+++ installers/jailkit.sh
@@ -22,7 +22,7 @@ mkdir -p $chroot_path
 jk_init $chroot_path netutils extendedshell jk_lsh openvpn ssh sftp 
 chroot_bin_path=$chroot_path/bin/
 echo "Copying binaries for JailKit"
-binaries_array=("xterm" "ls" "ln" "date" "rm" "rmdir" "mysql" "php56" "php70" "php71" "php72" "php73" "php74" "php80" "php81" "git" "wget" "curl" "nano" "stty" "grep" "find" "clear" "du" "cp" "mv" "touch" "cat" "whoami" "tee" "free" "gdb" "mkdir" "git-shell" "git-receive-pack" "git-upload-archive" "git-upload-pack" "/usr/lib/git-core/git-remote-https" "ping"  "ssh" "sftp" "sed" "awk" "tr" "tail" "sort" "less" "head" "cut" "egrep" "uname" "uniq" "groups" "env")
+binaries_array=("xterm" "ls" "ln" "date" "rm" "rmdir" "mysql" "php56" "php70" "php71" "php72" "php73" "php74" "php80" "php81" "git" "wget" "curl" "nano" "stty" "grep" "find" "clear" "du" "cp" "mv" "touch" "cat" "whoami" "tee" "free" "gdb" "mkdir" "git-shell" "git-receive-pack" "git-upload-archive" "git-upload-pack" "/usr/lib/git-core/git-remote-https" "ping"  "ssh" "sftp" "sed" "awk" "tr" "tail" "sort" "less" "head" "cut" "egrep" "uname" "uniq" "groups" "env" "dirname" "sha256sum" "sha256" "sha1sum" "sha1")
 
 for binary in ${binaries_array[@]}; do
     cp "$(which $binary)" $chroot_bin_path
