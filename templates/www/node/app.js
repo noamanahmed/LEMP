8347cf94ec44c3032708e5213fe9ce1b5a7c27f6
Noaman <noamanahmd@gmail.com>
2022-04-26 00:10:04 +0500

    Quick Push bd087c9a112a24f924932685

--- templates/www/node/app.js
+++ templates/www/node/app.js
@@ -1,11 +1,16 @@
 const express = require('express')
+const http = require('http');
 const app = express()
 const port = {{port}}
+
+var server = http.createServer(app);
 
 app.get('/', (req, res) => {
     res.send('A starter app.js has been setup for you.!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+server.listen(port, '127.0.0.1');
+
+server.on('listening', function() {
+    console.log(`Example app listening on port http:\/\/${server.address().address}:${port}`)
 })
