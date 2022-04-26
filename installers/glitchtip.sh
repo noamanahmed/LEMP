15ea4fd33af0585f73a4680035311eda4caf84fd
Noaman <noamanahmd@gmail.com>
2022-04-26 15:51:11 +0500

    Quick Push 7c95a02712dd07f2997501e2

--- installers/glitchtip.sh
+++ installers/glitchtip.sh
@@ -61,8 +61,8 @@ poetry add gunicorn
 cd $DIR
 #Setup DB Users
 adduser glitchtip --disabled-login
-sudo -u postgres -c 'createuser glitchtip'
-sudo -u postgres -c 'createdb -O glitchtip glitchtip'
+su -u postgres -c 'createuser glitchtip'
+su -u postgres -c 'createdb -O glitchtip glitchtip'
 #Setup backend app itself
 mkdir -p $glitchtip_path/runtime
 cp $template_path/glitchtip/.env $glitchtip_path/
@@ -70,7 +70,7 @@ sed -i "s/{{domain}}/$hostname/" $glitchtip_path/.env
 sed -i "s/{{hash}}/$(openssl rand -hex 32)/" $glitchtip_path/.env
 
 chown -R glitchtip:glitchtip $glitchtip_path
-sudo -u glitchtip -c "'python $glitchtip_path/manage.py migrate'"
+su -u glitchtip -c "'python $glitchtip_path/manage.py migrate'"
 
 git clone https://gitlab.com/glitchtip/glitchtip-frontend.git $glitchtip_path/frontend
 npm install --prefix $glitchtip_path/frontend/
