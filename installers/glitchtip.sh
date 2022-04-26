40925d12091bb6b79ca061681350b92f29f25a20
Noaman <noamanahmd@gmail.com>
2022-04-26 13:57:23 +0500

    Quick Push 5073a2f6395e5c0ee1dd01ca

--- installers/glitchtip.sh
+++ installers/glitchtip.sh
@@ -27,21 +27,55 @@ then
 fi
 
 
+# Docker version is deprecated now
+# mkdir -p /opt/glitchtip
+# cp $template_path/glitchtip/docker-composer.yml /opt/glitchtip/
+# cp $template_path/glitchtip/glitchtip.service /etc/systemd/systemd/
 
-mkdir -p /opt/glitchtip
-cp $template_path/glitchtip/docker-composer.yml /opt/glitchtip/
-cp $template_path/glitchtip/glitchtip.service /etc/systemd/systemd/
+# nginx_vhost_file="/etc/nginx/sites-available/glitchtip.conf"
+# nginx_vhost_enabled="/etc/nginx/sites-enabled/glitchtip.conf"
+# cp $template_path/glitchtip/vhost.conf $nginx_vhost_file
+
+# sed -i "s/{{domain}}/$HOSTNAME/" $nginx_vhost_file
+
+# ln -s $nginx_vhost_file $nginx_vhost_enabled
+
+# systemctl daemon-reload
+# systemctl start glitchtip
+# systemctl enable glitchtip
+# systemctl reload nginx
+
+glitchtip_path=/opt/glitchtip
+
+#Install poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+source $HOME/.poetry/env
+
+#Initiate setup
+mkdir -p $glitchtip_path
+git clone https://gitlab.com/glitchtip/glitchtip-backend.git $glitchtip_path/backend.
+poetry install
+poetry remove uWSGI
+poetry add gunicorn
+#Setup DB Users
+adduser glitchtip --disabled-login
+sudo -u postgres createuser glitchtip
+sudo -u postgres createdb -O glitchtip glitchtip
+#Setup backend app itself
+mkdir -p $glitchtip_path/runtime
+cp $template_path/glitchtip/.env $glitchtip_path/runtime/
+sudo -u glitchtip -c 'python manage.py migrate'
+
+git clone https://gitlab.com/glitchtip/glitchtip-frontend.git $glitchtip_path/frontend
+npm install --prefix $glitchtip_path/frontend/
+npm install --prefix run build-prod
+
 
 nginx_vhost_file="/etc/nginx/sites-available/glitchtip.conf"
 nginx_vhost_enabled="/etc/nginx/sites-enabled/glitchtip.conf"
 cp $template_path/glitchtip/vhost.conf $nginx_vhost_file
 
-sed -i "s/{{domain}}/$HOSTNAME/" $nginx_vhost_file
+sed -i "s/{{domain}}/$hostname/" $nginx_vhost_file
 
 ln -s $nginx_vhost_file $nginx_vhost_enabled
-
-systemctl daemon-reload
-systemctl start glitchtip
-systemctl enable glitchtip
 systemctl reload nginx
-
