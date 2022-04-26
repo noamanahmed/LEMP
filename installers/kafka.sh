4fc05555ebba137cfc8d87e574a24cbfe5378e58
Noaman <noamanahmd@gmail.com>
2022-04-26 17:20:40 +0500

    Quick Push fa03b17e72d3ead783f6a068

--- installers/kafka.sh
+++ installers/kafka.sh
@@ -19,6 +19,8 @@ mkdir -p /opt/kafka
 curl "https://downloads.apache.org/kafka/3.1.0/kafka_2.12-3.1.0.tgz" -o /opt/kafka/kafka.tgz
 tar -xvzf /opt/kafka/kafka.tgz -C /opt/kafka/
 mv /opt/kafka/kafka_2.12-3.1.0/* /opt/kafka/
+rm -rf kafka_2.12-3.1.0
+rm -rf kafka.tgz
 mkdir -p /opt/kafka/logs/
 cp $template_path/kafka/kafka.service /etc/systemd/system/
 cp $template_path/kafka/zookeeper.service /etc/systemd/system/
@@ -27,5 +29,6 @@ systemctl daemon-reload
 systemctl start kafka
 systemctl enable zookeeper
 systemctl enable kafka
+
 
 
