93e14da231eaa38acb76ccc9bbe3f608b2a83fc7
Noaman <noamanahmd@gmail.com>
2022-04-29 01:27:36 +0500

    Quick Push a056efc5df21eae1152d2868

--- install.sh
+++ install.sh
@@ -390,4 +390,5 @@ seconds=$(echo "$end - $start" | bc)
 echo "Time Taken to install: "
 awk -v t=$seconds 'BEGIN{t=int(t*1000); printf "%d:%02d:%02d\n", t/3600000, t/60000%60, t/1000%60}'
 
+bash slack-notification -u $username -d $domain -m "Installation completed: $hostname Time Taken: $seconds " --success
 
