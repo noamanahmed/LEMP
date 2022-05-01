85b8e7942f8de7022f10a58196447e52a28227c0
Noaman <noamanahmd@gmail.com>
2022-05-01 05:39:29 +0500

    Quick Push 26694652923777e14188d5ac

--- installers/postfixadmin.sh
+++ installers/postfixadmin.sh
@@ -54,6 +54,8 @@ cp $template_path/postfixadmin/vhost.conf $nginx_vhost_file
 sed -i "s/{{domain}}/$HOSTNAME/" $nginx_vhost_file
 sed -i "s/{{username}}/$username/" $nginx_vhost_file
 sed -i "s/{{www_path}}/$(echo $user_root | sed 's/\//\\\//g')/" $nginx_vhost_file
+sed -i "s/{{user_root}}/$(echo $user_root | sed 's/\//\\\//g')/" $nginx_vhost_file
+
 ln -s $nginx_vhost_file $nginx_vhost_enabled
 systemctl reload nginx
 
