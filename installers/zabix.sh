b3df0cb3429550499408960f67a2d88d0685dd6b
Noaman <noamanahmd@gmail.com>
2022-05-02 15:45:40 +0500

    Quick Push 74071e3907ae5e6ec50b3e05

--- installers/zabix.sh
+++ installers/zabix.sh
@@ -15,4 +15,9 @@ fi
 wget https://repo.zabbix.com/zabbix/6.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.0-1+ubuntu20.04_all.deb -O /tmp/zabbix.deb
 dpkg -i /tmp/zabbix.deb
 apt update
-apt install zabbix-agent
+apt install zabbix-agent -qqy
+
+cp $template_path/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf
+server_ip=$(ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p')
+sed -i "s/{{server_ip}}/$server_ip/" $nginx_vhost_file
+systemctl restart zabbix-agent
