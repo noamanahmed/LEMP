831b1d43f892074d185d0c62628479fa7c7692ba
Noaman <noamanahmd@gmail.com>
2022-05-05 14:47:24 +0500

    Quick Push 56244a2886d08e36169322e5

--- installers/pgadmin.sh
+++ installers/pgadmin.sh
@@ -39,11 +39,17 @@ python$python -m pip install gunicorn
 cp $template_path/pgadmin/config_local.py /opt/$username/.virtualenv/lib/python$python/site-packages/pgadmin4/config_local.py
 python$python /opt/$username/.virtualenv/lib/python$python/site-packages/pgadmin4/setup.py
 #gunicorn --bind 127.0.0.1:7210 --chdir /opt/$username/.virtualenv/lib/python$python/site-packages/pgadmin4/  wsgi:pgAdmin4.wsgi
+
+chown -R $username:$username /var/lib/pgadmin4
+chown -R $username:$username /var/log/pgadmin4
+
+chown -R $username:$username $user_root
 cp $template_path/pgadmin/pgadmin.service /etc/systemd/system/
 sed -i "s/{{python}}/$python/" /etc/systemd/system/pgadmin.service
 sed -i "s/{{user_root}}/$(echo $user_root | sed 's/\//\\\//g')/g"  /etc/systemd/system/pgadmin.service
 systemctl daemon-reload
 systemctl restart pgadmin
+systemctl enable pgadmin
 
 nginx_vhost_file="/etc/nginx/apps-available/pgadmin.conf"
 nginx_vhost_enabled="/etc/nginx/apps-enabled/pgadmin.conf"
