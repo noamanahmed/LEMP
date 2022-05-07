d405a2195766f76859d5433b70bd8a1b4d81af32
Noaman <noamanahmd@gmail.com>
2022-05-08 00:21:32 +0500

    Quick Push 9fe4271cdc18208b48e3ba9d

--- installers/prereq.sh
+++ installers/prereq.sh
@@ -19,7 +19,7 @@ echo "Installing Pre Req Binaries"
 echo ""
 
 
-binaries_array=( "software-properties-common" "screen" "htop"  "nload" "curl" "wget" "git" "unrar" "unzip" "zip" "speedtest-cli gzip" "pv" "logrotate" "rig" "runuser" "gnupg" "ca-certificates" "gnupg2" "sqlite3" "jq" "bc" "xclip" "tree" "dnsutils")
+binaries_array=( "software-properties-common" "screen" "htop"  "nload" "curl" "wget" "git" "unrar" "unzip" "zip" "speedtest-cli gzip" "pv" "logrotate" "rig" "runuser" "gnupg" "ca-certificates" "gnupg2" "sqlite3" "jq" "bc" "xclip" "tree" "dnsutils" "lsof")
 
 for binary in ${binaries_array[@]}; do      
   if ! command -v $binary &> /dev/null
