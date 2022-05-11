82c98ec7e6547202794c0d3b7a309ac5b5e4693c
Noaman <noamanahmd@gmail.com>
2022-05-11 15:14:42 +0500

    Quick Push 902bef7704c566f81ce3e18e

--- installers/ssh.sh
+++ installers/ssh.sh
@@ -44,9 +44,9 @@ chown -R $username:$username /home/$username/.bashrc
 cp -rf $template_path/ssh/.ssh/* /root/.ssh/
 cp -rf $template_path/ssh/.profile /root/
 cp -rf $template_path/ssh/.bashrc /root/
-chown -R $username:$username /root/.ssh
-chown -R $username:$username /root/.profile
-chown -R $username:$username /root/.bashrc
+chown -R root:root /root/.ssh
+chown -R root:root /root/.profile
+chown -R root:root /root/.bashrc
 
 systemctl restart ssh
 systemctl restart sshd
