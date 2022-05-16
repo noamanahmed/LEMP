0ab4007a18252197876dfd99d2fffd07efc87885
Noaman Ahmed <nauman.h@allshorevirtualstaffing.com>
2022-05-16 23:13:22 +0500

    Quick Push 17491f5c5d74e18ab8e08449

--- installers/jailkit.sh
+++ installers/jailkit.sh
@@ -138,6 +138,7 @@ fi
 
 if [ -d "$HOME/.nvm" ]
 then
+    echo "Copying NodeJS libraries"
     export NVM_DIR="$HOME/.nvm"
     [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
     [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
