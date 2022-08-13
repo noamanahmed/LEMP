d442dfae7f1a5bea2d6b9756cf55244e278eebd7
Noaman <noamanahmd@gmail.com>
2022-08-13 10:59:05 +0500

    Quick Push e60bbae906c216cc24106d20

--- installers/phpmyadmin.sh
+++ installers/phpmyadmin.sh
@@ -97,7 +97,7 @@ cp $template_path/phpmyadmin/vhost.conf $nginx_vhost_file
 sed -i "s/{{domain}}/$HOSTNAME/" $nginx_vhost_file
 sed -i "s/{{username}}/$username/" $nginx_vhost_file
 sed -i "s/{{www_path}}/$(echo $www_root | sed 's/\//\\\//g')/" $nginx_vhost_file
-sed -i "s/{{user_root}}/$(echo $user_root | sed 's/\//\\\//g')/" $nginx_vhost_file
+sed -i "s/{{user_root}}/$(echo $user_root/wwww | sed 's/\//\\\//g')/" $nginx_vhost_file
 
 ln -s $nginx_vhost_file $nginx_vhost_enabled
 systemctl reload nginx
