4ed619410f55022db9c095d82693563298a7d642
Noaman <noamanahmd@gmail.com>
2022-09-26 00:36:36 +0500

    Quick Push ae23ed667c024fe9f31b5c1f

--- installers/cron.sh
+++ installers/cron.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 DIR=$(dirname "${BASH_SOURCE[0]}") 
 DIR=$(realpath "${DIR}") 
 
 template_path="$(cd $DIR/../ && pwd)/templates"
+source $DIR/../includes/helpers.sh
 
 
 if [ "$EUID" -ne 0 ]
