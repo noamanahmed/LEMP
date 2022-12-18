c877dc2bd7b811770bca7897e0ae0453923f87d5
Noaman <noamanahmd@gmail.com>
2022-12-18 19:11:36 +0500

    Quick Push 86396e6343f31605be267534

--- installers/mailserver.sh
+++ installers/mailserver.sh
@@ -5,7 +5,7 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 DIR=$(realpath "${DIR}") 
 
 template_path="$(cd $DIR/../ && pwd)/templates"
-
+source $DIR/../includes/helpers.sh
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
@@ -29,7 +29,7 @@ systemctl restart nginx
 #TODO
 #https://www.linuxbabe.com/mail-server/postfix-amavis-spamassassin-clamav-ubuntu
 #https://www.linuxbabe.com/mail-server/block-email-spam-check-header-body-with-postfix-spamassassin
-#https://www.linuxbabe.com/mail-server/setting-up-dkim-and-spf
+
 
 
 touch $LEMP_FLAG_DIR/MAILSERVER_INSTALLED
